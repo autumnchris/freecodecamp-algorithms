3a5a42e45cc17e61fa1cada2c00c4cdb3b7b2e63
Autumn <autumncbullard@gmail.com>
2017-07-13 00:36:30 -0400

    Added solution for "Wherefore art thou"

---
+++ intermediate-algorithms/wherefore-art-thou.js
@@ -0,0 +1,25 @@
+// Wherefore art thou
+
+/*
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
+*/
+
+function whatIsInAName(collection, source) {
+
+  var arr = collection.filter(function(items) {
+
+  	for (var i in source) {
+
+  		if (source[i] !== items[i]) {
+  			return false;
+  		}
+  	}
+    return true;
+  });
+
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });

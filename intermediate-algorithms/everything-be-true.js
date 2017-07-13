3e7968249c7d94bef814bca38215356da5f1e287
Autumn <autumncbullard@gmail.com>
2017-07-13 02:28:54 -0400

    Added solution for "Everything Be True"

---
+++ intermediate-algorithms/everything-be-true.js
@@ -0,0 +1,27 @@
+// Everything Be True
+
+/*
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+Remember, you can access object properties through either dot notation or [] notation.
+*/
+
+function truthCheck(collection, pre) {
+
+  var arrCount = 0;
+
+  for (var i in collection) {
+
+    if (collection[i].hasOwnProperty(pre) && Boolean(collection[i][pre])) {
+      arrCount++;
+    }
+  }
+
+  if (arrCount === collection.length) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");

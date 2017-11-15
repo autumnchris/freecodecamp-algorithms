2c11d073dfad3856585ff2e17cc00af54dfca319
Autumn <autumncbullard@gmail.com>
2017-11-15 18:57:03 -0500

    Remove unnecessary spacing

--- intermediate-algorithms/sum-all-primes.js
+++ intermediate-algorithms/sum-all-primes.js
@@ -17,7 +17,7 @@ function sumPrimes(num) {
     var isPrime = true;
     for (var j = 2; j <= i; j++) {
 
-      if ( i % j === 0 && i !== j) {
+      if (i % j === 0 && i !== j) {
         isPrime = false;
       }
     }

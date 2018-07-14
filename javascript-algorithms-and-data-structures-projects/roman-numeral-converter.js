85411ed5bdbdd58680c2cca8fbb61ed031ae4204
Autumn <autumncbullard@gmail.com>
2018-07-14 16:27:00 -0400

    Turn the object into an array of objects

--- javascript-algorithms-and-data-structures-projects/roman-numeral-converter.js
+++ javascript-algorithms-and-data-structures-projects/roman-numeral-converter.js
@@ -7,26 +7,66 @@ All roman numerals answers should be provided in upper-case.
 */
 
 function convertToRoman(num) {
-  const obj = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1
-  };
-  return Object.keys(obj).reduce((result, key) => {
+  const arr = [
+      {
+          symbol: 'M',
+          value: 1000
+      },
+      {
+          symbol: 'CM',
+          value: 900
+      },
+      {
+          symbol: 'D',
+          value: 500
+      },
+      {
+          symbol: 'CD',
+          value: 400
+      },
+      {
+          symbol: 'C',
+          value: 100
+      },
+      {
+          symbol: 'XC',
+          value: 90
+      },
+      {
+          symbol: 'L',
+          value: 50
+      },
+      {
+          symbol: 'XL',
+          value: 40
+      },
+      {
+          symbol: 'X',
+          value: 10
+      },
+      {
+          symbol: 'IX',
+          value: 9
+      },
+      {
+          symbol: 'V',
+          value: 5
+      },
+      {
+          symbol: 'IV',
+          value: 4
+      },
+      {
+          symbol: 'I',
+          value: 1
+      }
+  ];
 
-    while (num >= obj[key]) {
-      result += key;
-      num -= obj[key];
+  return arr.reduce((result, key) => {
+
+    while (num >= key.value) {
+      result += key.symbol;
+      num -= key.value;
     }
     return result;
   }, '');
